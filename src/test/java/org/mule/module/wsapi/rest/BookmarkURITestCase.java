2098e62db52ef4de38d7f684866e8c236b70a219
Daniel Feist <dfeist@gmail.com>
2013-02-05 15:38:40 +0000

    Add failing test for bookmark uri

---
+++ src/test/java/org/mule/module/wsapi/rest/BookmarkURITestCase.java
@@ -0,0 +1,39 @@
+
+package org.mule.module.wsapi.rest;
+
+import static com.jayway.restassured.RestAssured.given;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.http.ContentType;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BookmarkURITestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/wsapi/rest/bookmark-uri-config.xml,  org/mule/module/wsapi/rest/flow-config.xml";
+    }
+
+    @Test
+    public void getBookmarkURIJSON() throws Exception
+    {
+        given().contentType(ContentType.JSON).expect().response().statusCode(404).when().get("/api");
+    }
+
+}
