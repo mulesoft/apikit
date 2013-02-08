4429c2193e0a9a2774a16edfc1af4bf83740e0da
svacas <svacas@gmail.com>
2013-02-08 09:48:38 -0300

    update module version

--- src/test/java/org/mule/module/wsapi/rest/config/resource/DocumentResourceTestCase.java
+++ src/test/java/org/mule/module/wsapi/rest/config/resource/DocumentResourceTestCase.java
@@ -1,8 +1,17 @@
 package org.mule.module.wsapi.rest.config.resource;
 
+import static junit.framework.Assert.assertTrue;
+
 import org.mule.tck.junit4.AbstractMuleTestCase;
+
+import org.junit.Test;
 
 public class DocumentResourceTestCase extends AbstractMuleTestCase
 {
 
+    @Test
+    public void mock()
+    {
+        assertTrue(true);
+    }
 }
