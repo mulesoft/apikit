df3cc3f679c71293c15ef0705cac57fbedb199a6
evangelinamartinezruizmoreno <evangelina.martinez@mulesource.com>
2013-07-19 17:04:28 -0300

    APIKIT-65: Improve scaffolder logging when yaml file is not valid

--- apikit-tools/apikit-maven-plugin/src/main/java/org/mule/tools/apikit/input/RAMLFilesParser.java
+++ apikit-tools/apikit-maven-plugin/src/main/java/org/mule/tools/apikit/input/RAMLFilesParser.java
@@ -1,5 +1,6 @@
 package org.mule.tools.apikit.input;
 
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.logging.Log;
 
 import org.mule.tools.apikit.misc.APIKitTools;
@@ -13,11 +14,14 @@ import org.raml.model.Resource;
 import org.raml.parser.loader.CompositeResourceLoader;
 import org.raml.parser.loader.DefaultResourceLoader;
 import org.raml.parser.loader.ResourceLoader;
+import org.raml.parser.rule.ValidationResult;
 import org.raml.parser.visitor.YamlDocumentBuilder;
+import org.raml.parser.visitor.YamlDocumentValidator;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.*;
 
@@ -31,45 +35,26 @@ public class RAMLFilesParser {
         this.generationModelProvider = generationModelProvider;
         List<File> processedFiles = new ArrayList<File>();
         for (Map.Entry<File, InputStream> fileInputStreamEntry : fileStreams.entrySet()) {
-            YamlDocumentBuilder<Raml> builderNodeHandler = new YamlDocumentBuilder<Raml>(Raml.class, new CompositeResourceLoader(new ResourceLoader()
-            {
-                @Override
-                public InputStream fetchResource(String resource)
-                {
-                    File file = new File(API_HOME, resource);
-                    if(file.exists()) {
-                        try {
-                            return new FileInputStream(file);
-                        } catch (FileNotFoundException e) {
-                            // Do nothing
-                        }
-                    }
-                    return null;
-                }
-            }, new ResourceLoader()
-            {
-                @Override
-                public InputStream fetchResource(String resource)
-                {
-                    File file = new File(resource);
-                    if(file.exists()) {
-                        try {
-                            return new FileInputStream(file);
-                        } catch(FileNotFoundException e) {
-                            // Do nothing
-                        }
-                    }
-                    return null;
-                }
-
-            }, new DefaultResourceLoader()));
+            String content;
             try {
-                Raml raml = builderNodeHandler.build(fileInputStreamEntry.getValue());
-                collectResources(fileInputStreamEntry.getKey(), entries, raml.getResources(), raml.getBaseUri());
-                processedFiles.add(fileInputStreamEntry.getKey());
-            } catch (Exception e) {
-                log.debug("Could not parse [" + fileInputStreamEntry.getKey() + "] as root yaml file.");
-                log.debug(e);
+                content = IOUtils.toString(fileInputStreamEntry.getValue());
+            } catch (IOException ioe) {
+                log.info("Error loading file " + fileInputStreamEntry.getKey().getName());
+                break;
+
+            }
+            ResourceLoader resourceLoader = getResourceLoader();
+
+            if(isValidYaml(fileInputStreamEntry.getKey().getName(), content, resourceLoader, log)) {
+                YamlDocumentBuilder<Raml> builderNodeHandler = new YamlDocumentBuilder<Raml>(Raml.class, resourceLoader);
+                try {
+                    Raml raml = builderNodeHandler.build(content);
+                    collectResources(fileInputStreamEntry.getKey(), entries, raml.getResources(), raml.getBaseUri());
+                    processedFiles.add(fileInputStreamEntry.getKey());
+                } catch (Exception e) {
+                    log.debug("Could not parse [" + fileInputStreamEntry.getKey() + "] as root yaml file.");
+                    log.debug(e);
+                }
             }
 
         }
@@ -81,6 +66,57 @@ public class RAMLFilesParser {
         }
     }
 
+    private boolean isValidYaml(String fileName, String content, ResourceLoader resourceLoader, Log log) {
+        YamlDocumentValidator yamlValidator = new YamlDocumentValidator(Raml.class, resourceLoader);
+        List<ValidationResult> validationResults =yamlValidator.validate(content);
+        if(validationResults != null && !validationResults.isEmpty()) {
+            log.info("File '" + fileName + "' is not a valid yaml file. See following error(s): ");
+            int errorCount = 1;
+            for(ValidationResult validationResult : validationResults) {
+                log.info("Error " + errorCount + ": " + validationResult.toString());
+                errorCount++;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private ResourceLoader getResourceLoader() {
+        return new CompositeResourceLoader(new ResourceLoader()
+        {
+            @Override
+            public InputStream fetchResource(String resource)
+            {
+                File file = new File(API_HOME, resource);
+                if(file.exists()) {
+                    try {
+                        return new FileInputStream(file);
+                    } catch (FileNotFoundException e) {
+                        // Do nothing
+                    }
+                }
+                return null;
+            }
+        }, new ResourceLoader()
+        {
+            @Override
+            public InputStream fetchResource(String resource)
+            {
+                File file = new File(resource);
+                if(file.exists()) {
+                    try {
+                        return new FileInputStream(file);
+                    } catch(FileNotFoundException e) {
+                        // Do nothing
+                    }
+                }
+                return null;
+            }
+
+        }, new DefaultResourceLoader());
+    }
+
+
     void collectResources(File filename, Set<ResourceActionPair> resources, Map<String, Resource> resourceMap, String baseUri) {
         for (Resource resource : resourceMap.values()) {
             for (Action action : resource.getActions().values()) {
@@ -90,7 +126,7 @@ public class RAMLFilesParser {
                         action.getType().toString());
                 generationModelProvider.add(api, resourceActionPair, resource, action);
                 resources.add(resourceActionPair);
-            }
+             }
 
             collectResources(filename, resources, resource.getResources(), baseUri);
         }
