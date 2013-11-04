35b7baecd5504a624f954a9edbe4f2fe51db303b
Santiago Vacas <svacas@gmail.com>
2013-11-04 16:00:18 -0300

    update license to CPAL

--- apikit-tools/apikit-maven-plugin/src/main/java/org/mule/tools/apikit/model/API.java
+++ apikit-tools/apikit-maven-plugin/src/main/java/org/mule/tools/apikit/model/API.java
@@ -1,11 +1,14 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
 package org.mule.tools.apikit.model;
 
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.lang.Validate;
-
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
+
+import org.apache.commons.io.FilenameUtils;
 
 public class API {
 
