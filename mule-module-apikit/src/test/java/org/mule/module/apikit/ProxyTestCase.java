aadb8b41e11eec30822690f60c1baef84d3bea59
Santiago Vacas <svacas@gmail.com>
2014-06-06 18:19:01 -0300

    fix proxy test

--- mule-module-apikit/src/test/java/org/mule/module/apikit/ProxyTestCase.java
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/ProxyTestCase.java
@@ -15,7 +15,6 @@ import org.mule.test.infrastructure.deployment.AbstractFakeMuleServerTestCase;
 
 import com.jayway.restassured.RestAssured;
 
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -41,12 +40,12 @@ public class ProxyTestCase extends AbstractFakeMuleServerTestCase
     }
 
     @Test
-    @Ignore //TODO cannot find raml file when running in bamboo
     public void proxy() throws Exception
     {
         muleServer.deployAppFromClasspathFolder("org/mule/module/apikit/proxy/proxied-app", "proxied");
-        muleServer.deployAppFromClasspathFolder("org/mule/module/apikit/proxy/app", "proxy");
         muleServer.start();
+        muleServer.deployAppFromClasspathFolder("org/mule/module/apikit/proxy/app", "proxy");
+        muleServer.assertDeploymentSuccess("proxy");
         getOnLeaguesJson();
         getOnLeagueJson();
         notAcceptable();
