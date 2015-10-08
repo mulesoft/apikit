4f6f92cd41413a4c1ac383393ce436597dc6ec4c
svacas <svacas@gmail.com>
2015-10-08 14:30:52 -0300

    APIKIT-408: APIKit vulnerable to XXE attack

---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/schema/XxeAttackTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.schema;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.internal.matchers.StringContains.containsString;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.response.Response;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class XxeAttackTestCase extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "org/mule/module/apikit/schema/xxe-attack-config.xml";
+    }
+
+    @Test
+    public void xxeAttack() throws Exception
+    {
+        Response post = given().log().all()
+                .body("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><!DOCTYPE foo [<!ENTITY xxead812 SYSTEM \"src/test/resources/org/mule/module/apikit/schema/twin-cam.yaml\"> ]><a>&xxead812;</a>")
+                .contentType("application/xml")
+                .expect().statusCode(400)
+                .when().post("/api/test");
+        String response = post.getBody().asString();
+        assertThat(response, not(containsString("League Schema")));
+    }
+
+}
