b4a143890509fe511cf4a1a070c89c24e4b14f18
Federico Donnarumma <federico.donnarumma@mulesoft.com>
2015-10-20 17:59:27 -0300

    APIKIT-414: Adding changes to make pattern case insensitive

--- mule-module-apikit/src/main/java/org/mule/module/apikit/uri/TokenVariable.java
+++ mule-module-apikit/src/main/java/org/mule/module/apikit/uri/TokenVariable.java
@@ -95,7 +95,7 @@ public class TokenVariable extends TokenBase implements Token, Matchable
      */
     public boolean resolve(String expanded, Map<Variable, Object> values)
     {
-        values.put(this._var, URICoder.decode(expanded).replaceAll("%2F", "/"));
+        values.put(this._var, URICoder.decode(expanded).replaceAll("(?i)%2F", "/"));
         return true;
     }
 
