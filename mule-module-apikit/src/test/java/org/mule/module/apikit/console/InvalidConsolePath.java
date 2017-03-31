325476173628bf0c60e9324a5552355530945752
Lorena S. Daicich <lorenadaicich@gmail.com>
2017-03-31 11:51:16 -0300

    APIKIT-749: path to the RAML console defined in the "HTTP Listener" causes StringIndexOutOfBoundsException

---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/console/InvalidConsolePath.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.module.apikit.console;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import static com.jayway.restassured.RestAssured.given;
+
+public class InvalidConsolePath extends FunctionalTestCase
+{
+    @Rule
+    public DynamicPort listenerConsole = new DynamicPort("http.port");
+    @Rule
+    public DynamicPort secondListenerConsole = new DynamicPort("second.http.port");
+    @Rule
+    public DynamicPort inboundEndpointConsole = new DynamicPort("inboundEndpointConsole");
+    @Rule
+    public DynamicPort secondInboundEndpointConsole = new DynamicPort("secondInboundEndpointConsole");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/apikit/console/invalid-console-path.xml";
+    }
+
+    @Test
+    public void getConsoleWithInvalidPathWithListener() throws Exception
+    {
+        RestAssured.port = listenerConsole.getNumber();
+        given().expect()
+                .statusCode(500)
+                .when().get("/console/");
+    }
+
+    @Test
+    public void getConsoleWithInboundEndpoint() throws Exception
+    {
+        RestAssured.port = inboundEndpointConsole.getNumber();
+        given().expect()
+                .statusCode(200)
+                .when().get("/console/");
+    }
+
+    @Test
+    public void getSecondConsoleWithInvalidPathWithListener() throws Exception
+    {
+        RestAssured.port = secondListenerConsole.getNumber();
+        given().expect()
+                .statusCode(500)
+                .when().get("/console");
+    }
+
+    @Test
+    public void getSecondConsoleWithInboundEndpoint() throws Exception
+    {
+        RestAssured.port = secondInboundEndpointConsole.getNumber();
+        given().expect()
+                .statusCode(200)
+                .when().get("/console");
+    }
+
+}
