b0a6d052c1425d5661773610edb618f7f8012a43
svacas <svacas@gmail.com>
2017-05-02 15:57:15 -0300

    Test that static resource wins over parameterized sibling when routing

---
+++ mule-module-apikit/src/test/java/org/mule/module/apikit/AmbiguousResourcePathTestCase.java
@@ -0,0 +1,63 @@
+package org.mule.module.apikit;
+
+
+import static com.jayway.restassured.RestAssured.given;
+
+import org.mule.tck.junit4.FunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class AmbiguousResourcePathTestCase extends FunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 600000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigFile()
+    {
+        return "org/mule/module/apikit/ambiguous/flow-config.xml";
+    }
+
+    @Test
+    public void staticResourceA() throws Exception
+    {
+        given().header("Accept", "text/plain")
+                .expect().response().statusCode(200)
+                .when().get("/api/root/resourceA");
+    }
+
+    @Test
+    public void stringParamResource() throws Exception
+    {
+        given().header("Accept", "text/plain")
+                .expect().response().statusCode(201)
+                .when().get("/api/root/resourceC");
+    }
+
+    @Test
+    public void staticResourceB() throws Exception
+    {
+        given().header("Accept", "text/plain")
+                .expect().response().statusCode(202)
+                .when().get("/api/root/resourceB");
+    }
+
+}
