100efeaecfa583de1bb4530d5c72c77c1b37bbe2
Federico Amdan <famdan@mulesoft.com>
2017-05-09 20:05:23 -0300

    Router: Replacing Intercepting processor with Processor

--- mule-module-apikit/src/main/java/org/mule/module/apikit/Router.java
+++ mule-module-apikit/src/main/java/org/mule/module/apikit/Router.java
@@ -15,18 +15,19 @@ import org.mule.module.apikit.validation.AttributesValidatior;
 import org.mule.module.apikit.validation.BodyValidator;
 import org.mule.raml.interfaces.model.IResource;
 import org.mule.runtime.api.exception.MuleException;
+import org.mule.runtime.api.meta.AbstractAnnotatedObject;
 import org.mule.runtime.core.api.DefaultMuleException;
 import org.mule.runtime.core.api.Event;
 import org.mule.runtime.core.api.construct.Flow;
+import org.mule.runtime.core.api.processor.Processor;
 import org.mule.runtime.core.exception.TypedException;
-import org.mule.runtime.core.processor.AbstractInterceptingMessageProcessor;
 
 import java.util.HashMap;
 import java.util.concurrent.ExecutionException;
 
 import javax.inject.Inject;
 
-public class Router extends AbstractInterceptingMessageProcessor
+public class Router extends AbstractAnnotatedObject implements Processor
 {
     @Inject
     private ApikitRegistry registry;
@@ -41,7 +42,6 @@ public class Router extends AbstractInterceptingMessageProcessor
     {
         Configuration config = registry.getConfiguration(getConfigRef());
         event = EventHelper.addVariable(event, config.getOutboundHeadersMapName(), new HashMap<>());
-//        event = EventHelper.addVariable(event, config.getHttpStatusVarName(), "201");
 
         HttpRequestAttributes attributes = ((HttpRequestAttributes)event.getMessage().getAttributes().getValue());
 
