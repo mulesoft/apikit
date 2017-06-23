8c088faaf09ffae9a460180b64870606eb19bc8c
Federico Amdan <famdan@mulesoft.com>
2017-06-23 16:26:03 -0300

    Extension model: Changing version to 1.0.0-BETA

--- mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
@@ -31,7 +31,7 @@ public class ApikitExtensionLoadingDelegate implements ExtensionLoadingDelegate
     public static final String PREFIX_NAME = "apikit";
     public static final String EXTENSION_DESCRIPTION = "APIKit plugin";
     public static final String VENDOR = "Mulesoft";
-    public static final String VERSION = "4.0.0";
+    public static final String VERSION = "1.0.0-BETA";
     public static final MuleVersion MIN_MULE_VERSION = new MuleVersion("4.0");
     public static final String XSD_FILE_NAME = "mule-apikit.xsd";
     private static final String UNESCAPED_LOCATION_PREFIX = "http://";
