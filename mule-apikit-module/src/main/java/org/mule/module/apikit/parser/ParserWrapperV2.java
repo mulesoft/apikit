1504021619e9dda07edf39fa1cab61f4c07977d6
Federico Amdan <famdan@mulesoft.com>
2017-07-14 00:40:26 -0300

    APIKIT-888: Includes with absolute paths were not resolved

--- mule-apikit-module/src/main/java/org/mule/module/apikit/parser/ParserWrapperV2.java
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/parser/ParserWrapperV2.java
@@ -12,11 +12,14 @@ import org.mule.module.apikit.injector.RamlUpdater;
 import org.mule.raml.implv2.ParserV2Utils;
 import org.mule.raml.interfaces.model.IRaml;
 
+import java.io.File;
 import java.io.InputStream;
 import java.util.List;
 
+import org.raml.parser.loader.CompositeResourceLoader;
 import org.raml.v2.api.loader.DefaultResourceLoader;
 import org.raml.v2.api.loader.ResourceLoader;
+import org.raml.v2.api.loader.RootRamlResourceLoader;
 import org.raml.v2.internal.utils.StreamUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -30,7 +33,15 @@ public class ParserWrapperV2 implements ParserWrapper {
 
   public ParserWrapperV2(String ramlPath) {
     this.ramlPath = ramlPath;
-    this.resourceLoader = new DefaultResourceLoader();
+    if (ramlPath != null && new File(ramlPath).getParent() != null)
+    {
+      File ramlFile = new File(Thread.currentThread().getContextClassLoader().getResource(ramlPath).getFile());
+      this.resourceLoader = new org.raml.v2.api.loader.CompositeResourceLoader(new RootRamlResourceLoader(ramlFile.getParentFile()), new DefaultResourceLoader());
+    }
+    else
+    {
+      this.resourceLoader = new DefaultResourceLoader();
+    }
   }
 
   @Override
