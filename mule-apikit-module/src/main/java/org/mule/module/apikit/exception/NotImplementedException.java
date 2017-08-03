817214dcddc8ae6ed6dff443b43960c686c4ec63
diego.macias <diego.macias@dmacias.local>
2017-08-03 17:49:44 -0300

    APIKIT-901 Resource found in raml but not in config, throws 500 server error

---
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/exception/NotImplementedException.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit.exception;
+
+import org.mule.module.apikit.api.exception.MuleRestException;
+
+public class NotImplementedException extends MuleRestException
+{
+  public static final String STRING_REPRESENTATION = "APIKIT:NOT_IMPLEMENTED";
+
+  public NotImplementedException(){
+    super("Not implemented");
+  }
+
+  @Override
+  public String getStringRepresentation()
+  {
+    return STRING_REPRESENTATION;
+  }
+}
