0f5b85be5ac3bfa5b038e2c7871726b80b80ed03
Lorena S. Daicich <lorenadaicich@gmail.com>
2017-08-04 14:54:30 -0300

    Adding missing Headers

--- mule-apikit-metadata/src/test/java/org/mule/module/metadata/utils/MockedApplicationModel.java
+++ mule-apikit-metadata/src/test/java/org/mule/module/metadata/utils/MockedApplicationModel.java
@@ -1,9 +1,11 @@
-package org.mule.module.metadata.utils;/*
+/*
  * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
  * The software in this package is published under the terms of the CPAL v1.0
  * license, a copy of which has been included with this distribution in the
  * LICENSE.txt file.
  */
+package org.mule.module.metadata.utils;
+
 import static java.util.Collections.emptyList;
 import static java.util.Collections.emptySet;
 import static org.mule.runtime.config.spring.api.XmlConfigurationDocumentLoader.noValidationDocumentLoader;
