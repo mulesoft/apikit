65fd1716c835d935f1480e1f5e3d5db8392f00be
Federico Amdan <famdan@mulesoft.com>
2017-08-12 19:57:16 -0300

    APIKIT-807: Retrieving root raml v1 from api endpoint

---
+++ mule-apikit-module/src/test/java/org/mule/module/apikit/RouterRamlProcessorsAfterRouterTestCase.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+import org.mule.test.runner.ArtifactClassLoaderRunnerConfig;
+import org.junit.Rule;
+import org.junit.Test;
+
+
+import com.jayway.restassured.RestAssured;
+
+@ArtifactClassLoaderRunnerConfig
+public class RouterRamlProcessorsAfterRouterTestCase extends MuleArtifactFunctionalTestCase
+{
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("serverPort");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/apikit/router-raml/processors-after-router.xml";
+    }
+
+    @Test
+    public void getRamlV1() throws Exception
+    {
+        given().header("Accept", "application/raml+yaml")
+                .expect()
+                .response().body(containsString("#%RAML 0.8"))
+                .statusCode(200)
+                .when().get("/api");
+    }
+}
