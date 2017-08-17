16e9d4010318a0d8825a6f89f8f2b653b238ec30
Raul Tarda <raul.tarda@mulesoft.com>
2017-08-17 14:49:53 -0300

    APIKIT-702 APIKit fails on XML starting with BOM and containing tab character

---
+++ mule-apikit-module/src/test/java/BOMTestCase.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.runtime.core.api.util.IOUtils;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import com.jayway.restassured.RestAssured;
+
+import java.io.InputStream;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class BOMTestCase extends MuleArtifactFunctionalTestCase
+{
+
+    @Rule
+    public DynamicPort serverPort = new DynamicPort("http.port");
+
+    @Override
+    public int getTestTimeoutSecs()
+    {
+        return 6000;
+    }
+
+    @Override
+    protected void doSetUp() throws Exception
+    {
+        RestAssured.port = serverPort.getNumber();
+        super.doSetUp();
+    }
+
+    @Override
+    protected String getConfigResources()
+    {
+        return "org/mule/module/apikit/unicode/bom-test/bom-test-config.xml";
+    }
+
+    @Test
+    public void trimBomInXMLPayload()
+    {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("org/mule/module/apikit/unicode/bom-test/api/examples/person-example.xml");
+        byte[] body = IOUtils.toByteArray(inputStream);
+
+        given()
+                .contentType("application/xml")
+                .body(body)
+                .expect()
+                    .statusCode(200)
+                    .body(is("{\"status\": \"ok\"}"))
+                .when()
+                    .post("/api/target");
+    }
+}
