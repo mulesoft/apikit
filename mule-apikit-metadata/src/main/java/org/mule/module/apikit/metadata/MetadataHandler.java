a2b5637ad34d18cf91ec2d5e2719b98ad6f6bf95
Diego Larralde <diego.larralde@gmail.com>
2017-08-22 17:15:57 -0300

    APIKIT-961 Log removed

--- mule-apikit-metadata/src/main/java/org/mule/module/apikit/metadata/MetadataHandler.java
+++ mule-apikit-metadata/src/main/java/org/mule/module/apikit/metadata/MetadataHandler.java
@@ -32,7 +32,6 @@ public class MetadataHandler {
     final RamlCoordinate coordinate = modelWrapper.getRamlCoordinatesForFlow(flowName);
 
     if (coordinate == null) {
-      notifier.error(format("There is no metadata for flow '%s'", flowName));
       return empty();
     }
 
