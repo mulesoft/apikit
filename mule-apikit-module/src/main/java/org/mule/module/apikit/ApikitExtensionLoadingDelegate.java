60d167b86eda56a68dd1fc064c44c642e22e5d97
Diego Federico Macías <diego.macias@Diegos-MacBook-Pro.local>
2017-12-15 15:46:54 -0300

    Prepare for release 1.0.1

--- mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
@@ -32,7 +32,7 @@ public class ApikitExtensionLoadingDelegate implements ExtensionLoadingDelegate 
   public static final String PREFIX_NAME = "apikit";
   public static final String EXTENSION_DESCRIPTION = "APIKit plugin";
   public static final String VENDOR = "Mulesoft";
-  public static final String VERSION = "1.0.0";
+  public static final String VERSION = "1.0.1";
   public static final String XSD_FILE_NAME = "mule-apikit.xsd";
   private static final String UNESCAPED_LOCATION_PREFIX = "http://";
   private static final String SCHEMA_LOCATION = "www.mulesoft.org/schema/mule/mule-apikit";
