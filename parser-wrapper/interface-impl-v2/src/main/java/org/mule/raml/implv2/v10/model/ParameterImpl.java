f9de0dd3932ff69fd35a2498281704d7339cc50d
Paca <fuentespaca@gmail.com>
2018-06-14 12:29:26 -0300

    SE-6809 queryString supported. (#340)
    * SE-6809 queryString supported.
    
    * Property not required.

--- parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/v10/model/ParameterImpl.java
+++ parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/v10/model/ParameterImpl.java
@@ -6,25 +6,49 @@
  */
 package org.mule.raml.implv2.v10.model;
 
+import com.google.common.base.Function;
 import org.mule.raml.interfaces.model.parameter.IParameter;
 import org.raml.v2.api.model.common.ValidationResult;
 import org.raml.v2.api.model.v10.datamodel.ArrayTypeDeclaration;
 import org.raml.v2.api.model.v10.datamodel.ExampleSpec;
+import org.raml.v2.api.model.v10.datamodel.ObjectTypeDeclaration;
 import org.raml.v2.api.model.v10.datamodel.StringTypeDeclaration;
 import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
+import org.raml.v2.internal.impl.v10.type.TypeId;
 
+import javax.annotation.Nullable;
+import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.collect.Collections2.transform;
+import static com.google.common.collect.Sets.newHashSet;
+import static org.raml.v2.internal.impl.v10.type.TypeId.ARRAY;
+import static org.raml.v2.internal.impl.v10.type.TypeId.OBJECT;
 
 public class ParameterImpl implements IParameter
 {
 
     private TypeDeclaration typeDeclaration;
+    private Collection<String> scalarTypes;
 
     public ParameterImpl(TypeDeclaration typeDeclaration)
     {
         this.typeDeclaration = typeDeclaration;
+
+        Set<TypeId> typeIds = newHashSet(TypeId.values());
+        typeIds.remove(OBJECT);
+        typeIds.remove(ARRAY);
+
+        scalarTypes = transform(typeIds, new Function<TypeId, String>() {
+            @Nullable
+            @Override
+            public String apply(@Nullable TypeId input) {
+                return input.getType();
+            }
+        });
     }
 
     @Override
@@ -108,5 +132,20 @@ public class ParameterImpl implements IParameter
     @Override
     public boolean isStringArray() {
         return isArray() && ((ArrayTypeDeclaration) typeDeclaration).items() instanceof StringTypeDeclaration;
+    }
+
+    @Override
+    public boolean isScalar() {
+        return scalarTypes.contains(typeDeclaration.type());
+    }
+
+    @Override
+    public boolean isFacetArray(String facet) {
+        if (typeDeclaration instanceof ObjectTypeDeclaration) {
+            for (TypeDeclaration type : ((ObjectTypeDeclaration) typeDeclaration).properties()) {
+                if (type.name().equals(facet)) return type instanceof ArrayTypeDeclaration;
+            }
+        }
+        return false;
     }
 }
