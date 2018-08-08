9888c8f52735f7fdd74c609a2dff12be353ec857
dfmacias <elmellidiego@gmail.com>
2018-08-08 16:18:48 -0300

    minor: fixing format

--- mule-apikit-module/src/test/java/org/mule/module/apikit/helpers/CharsetUtilsTestCase.java
+++ mule-apikit-module/src/test/java/org/mule/module/apikit/helpers/CharsetUtilsTestCase.java
@@ -11,8 +11,10 @@ import org.junit.Test;
 import org.mule.module.apikit.CharsetUtils;
 
 public class CharsetUtilsTestCase {
-    @Test public void headerCharset() {
-        String charset = CharsetUtils.getCharset("application/json; charset=windows-1252; skipnullon=\"everywhere\"");
-        Assert.assertEquals(charset, "windows-1252");
-    }
+
+  @Test
+  public void headerCharset() {
+    String charset = CharsetUtils.getCharset("application/json; charset=windows-1252; skipnullon=\"everywhere\"");
+    Assert.assertEquals(charset, "windows-1252");
+  }
 }
