c733588b5225df497596943b0b724f6c3fccca6e
Diego Federico Macías <elmellidiego@gmail.com>
2018-08-17 15:15:50 -0300

    APIKIT-1436: Apikit not using example from raml (#360)

--- parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/v10/model/MimeTypeImpl.java
+++ parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/v10/model/MimeTypeImpl.java
@@ -6,18 +6,19 @@
  */
 package org.mule.raml.implv2.v10.model;
 
-import static org.mule.raml.implv2.v10.model.RamlImpl10V2.getTypeAsString;
-
 import org.mule.raml.interfaces.model.IMimeType;
 import org.mule.raml.interfaces.model.parameter.IParameter;
+import org.raml.v2.api.model.common.ValidationResult;
+import org.raml.v2.api.model.v10.datamodel.ExampleSpec;
+import org.raml.v2.api.model.v10.datamodel.ObjectTypeDeclaration;
+import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
+import org.raml.v2.api.model.v10.datamodel.UnionTypeDeclaration;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.raml.v2.api.model.common.ValidationResult;
-import org.raml.v2.api.model.v10.datamodel.ExampleSpec;
-import org.raml.v2.api.model.v10.datamodel.TypeDeclaration;
+import static org.mule.raml.implv2.v10.model.RamlImpl10V2.getTypeAsString;
 
 public class MimeTypeImpl implements IMimeType
 {
@@ -36,27 +37,71 @@ public class MimeTypeImpl implements IMimeType
     }
 
     @Override
-    public String getExample()
-    {
-        ExampleSpec example = typeDeclaration.example();
-        if (example != null && example.value() != null)
-        {
-            return example.value();
+    public String getExample() {
+        final ExampleSpec example = getExample(typeDeclaration);
+        return example != null ? example.value() : null;
+    }
+
+    private static ExampleSpec getExample(TypeDeclaration typeDeclaration) {
+        final ExampleSpec localExample = getExampleFromLocal(typeDeclaration);
+        if (localExample != null) return localExample;
+
+        if (typeDeclaration instanceof ObjectTypeDeclaration) {
+            return getExampleFromParent((ObjectTypeDeclaration) typeDeclaration);
         }
+
+        if (typeDeclaration instanceof UnionTypeDeclaration) {
+            final List<TypeDeclaration> parentTypes = ((UnionTypeDeclaration) typeDeclaration).of();
+            for (TypeDeclaration parentType : parentTypes) {
+                final ExampleSpec example = getExample(parentType);
+                if (example != null) return example;
+            }
+        }
+
+        return null;
+    }
+
+    private static ExampleSpec getExampleFromLocal(TypeDeclaration typeDeclaration) {
+        final ExampleSpec example = typeDeclaration.example();
+        if (example != null && example.value() != null) return example;
+
         List<ExampleSpec> examples = typeDeclaration.examples();
-        if (examples != null && !examples.isEmpty())
-        {
-            if (examples.get(0).value() != null)
-            {
-                return examples.get(0).value();
+        if (examples != null && !examples.isEmpty()) {
+            for (ExampleSpec e : examples) {
+                if (e.value() != null) return e;
+            }
+        }
+
+        return null;
+    }
+
+    private static ExampleSpec getExampleFromParent(ObjectTypeDeclaration typeDeclaration) {
+        if (!hasLocalProperties(typeDeclaration)) {
+            for (TypeDeclaration declaration : typeDeclaration.parentTypes()) {
+                final ExampleSpec parentExample = getExample(declaration);
+                if (parentExample != null) return parentExample;
             }
         }
         return null;
     }
 
+    private static boolean hasLocalProperties(ObjectTypeDeclaration typeDeclaration) {
+        final List<TypeDeclaration> parentTypes = typeDeclaration.parentTypes();
+        if (parentTypes.isEmpty()) return true;
+
+        final int propertiesCount = typeDeclaration.properties().size();
+
+        for (TypeDeclaration parentType : parentTypes) {
+            if (parentType instanceof ObjectTypeDeclaration && ((ObjectTypeDeclaration) parentType).properties().size() < propertiesCount) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     @Override
-    public String getSchema()
-    {
+    public String getSchema() {
         return getTypeAsString(typeDeclaration);
     }
 
