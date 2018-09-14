24eb7e3b773ac0c6ecf544528b06e5faa2c80f98
Juan Brasca <juan.brasca@gmail.com>
2018-09-14 16:43:18 -0300

    APIKIT-1556: Enhace tests

--- mule-apikit-tools/mule-apikit-scaffolder/src/test/java/org/mule/tools/apikit/ScaffolderApiSyncTest.java
+++ mule-apikit-tools/mule-apikit-scaffolder/src/test/java/org/mule/tools/apikit/ScaffolderApiSyncTest.java
@@ -8,118 +8,127 @@ package org.mule.tools.apikit;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.maven.model.Dependency;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 import org.mockito.Mockito;
-import org.mule.tools.apikit.misc.FileListUtils;
-import org.mule.tools.apikit.model.RuntimeEdition;
 import org.mule.tools.apikit.model.ScaffolderResourceLoader;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mule.tools.apikit.Helper.countOccurences;
-import static org.mule.tools.apikit.model.RuntimeEdition.CE;
 import static org.mule.tools.apikit.model.RuntimeEdition.EE;
 
 public class ScaffolderApiSyncTest {
 
-  public final static String DEFAULT_MULE_VERSION = "4.0.0";
-  public final static RuntimeEdition DEFAULT_RUNTIME_EDITION = EE;
+  private final static String MULE_4_VERSION = "4.0.0";
+  private final static ScaffolderResourceLoader scaffolderResourceLoaderMock = Mockito.mock(ScaffolderResourceLoader.class);
+
+  private final Dependency dependency = createDependency("com.mycompany", "raml-api", "1.0.0", "raml", "zip");
+  private final static String ROOT_RAML_RESOURCE_URL = "resource::com.mycompany:raml-api:1.0.0:raml:zip:";
+  private final static String DEPENDENCIES_RESOURCE_URL = "/exchange_modules/com.mycompany/raml-library/1.1.0/raml-fragment/zip/";
+
   @Rule
   public TemporaryFolder folder = new TemporaryFolder();
-  private FileListUtils fileListUtils = new FileListUtils();
-
-  @Test
-  public void testSimpleGeneration() throws IOException {
-    ScaffolderResourceLoader scaffolderResourceLoaderMock = Mockito.mock(ScaffolderResourceLoader.class);
-    final String exchangeJsonResourceURL = "resource::com.mycompany:raml-api:1.0.0:raml:zip:exchange.json";
-    final String rootRamlResourceURL = "resource::com.mycompany:raml-api:1.0.0:raml:zip:simpleV10.raml";
-
-    Mockito.doReturn(getInputStream("src/test/resources/scaffolder/exchange2.json")).when(scaffolderResourceLoaderMock)
-        .getResourceAsStream(exchangeJsonResourceURL);
-    Mockito.doReturn(getInputStream("src/test/resources/scaffolder/simpleV10.raml")).when(scaffolderResourceLoaderMock)
-        .getResourceAsStream(rootRamlResourceURL);
-
-    List<Dependency> dependencyList = new ArrayList<>();
-    dependencyList.add(createDependency("com.mycompany", "raml-api", "1.0.0", "raml", "zip"));
-    List<File> xmls = Arrays.asList();
-    File muleXmlOut = folder.newFolder("mule-xml-out");
-
-    new ScaffolderAPI().run(dependencyList, scaffolderResourceLoaderMock, muleXmlOut, null, DEFAULT_MULE_VERSION,
-                            DEFAULT_RUNTIME_EDITION);
-
-    File muleXmlSimple = new File(muleXmlOut, "resource::com.mycompany:raml-api:1.0.0:raml:zip:simpleV10.xml");
-
-    assertTrue(muleXmlSimple.exists());
-    String s = IOUtils.toString(new FileInputStream(muleXmlSimple));
-
-    assertEquals(1, countOccurences(s, "http:listener-config name=\"resource::com.mycompany:raml-api:1.0.0:raml:zip:simpleV10"));
-    assertEquals(1, countOccurences(s, "http:listener-connection host=\"0.0.0.0\" port=\"8081\""));
-    assertEquals(2, countOccurences(s, "http:listener "));
-    assertEquals(0, countOccurences(s, "interpretRequestErrors=\"true\""));
-    assertEquals(2, countOccurences(s, "http:response statusCode=\"#[vars.httpStatus default 200]\""));
-    assertEquals(2, countOccurences(s, "http:error-response statusCode=\"#[vars.httpStatus default 500]\""));
-    assertEquals(4, countOccurences(s, "#[vars.outboundHeaders default {}]"));
-    assertEquals(7, countOccurences(s, "<on-error-propagate"));
-    assertEquals(7, countOccurences(s, "<ee:message>"));
-    assertEquals(9, countOccurences(s, "<ee:variables>"));
-    assertEquals(10, countOccurences(s, "<ee:set-variable"));
-    assertEquals(2, countOccurences(s, "<ee:set-variable variableName=\"name\">attributes.uriParams.name</ee:set-variable>"));
-    assertEquals(1, countOccurences(s, "<ee:set-variable variableName=\"owner\">attributes.uriParams.owner</ee:set-variable>"));
-    assertEquals(7, countOccurences(s, "<ee:set-payload>"));
-    assertEquals(4, countOccurences(s, "http:body"));
-    assertEquals(2, countOccurences(s, "#[payload]"));
-    assertEquals(8, countOccurences(s, "http:headers"));
-    //    assertEquals(2, countOccurences(s, "get:\\:" + rootRamlResourceURL + "-config"));
-    //    assertEquals(2, countOccurences(s, "get:\\pet:" + rootRamlResourceURL + "-config"));
-    assertEquals(0, countOccurences(s, "extensionEnabled"));
-    assertEquals(1, countOccurences(s, "apikit:console"));
-    assertEquals(0, countOccurences(s, "consoleEnabled=\"false\""));
-    assertEquals(0, countOccurences(s, "#[NullPayload.getInstance()]"));
-    assertEquals(0, countOccurences(s, "#[null]"));
-    assertEquals(0,
-                 countOccurences(s,
-                                 "expression-component>mel:flowVars['variables.outboundHeaders default {}'].put('Content-Type', 'application/json')</expression-component>"));
-    assertEquals(0,
-                 countOccurences(s,
-                                 "set-variable variableName=\"variables.outboundHeaders default {}\" value=\"#[mel:new java.util.HashMap()]\" />"));
-    assertEquals(0, countOccurences(s, "exception-strategy"));
-    assertEquals(5, countOccurences(s, "<logger level=\"INFO\" message="));
-  }
 
   @Test
   public void testSimpleGenerationV08() throws IOException {
-    ScaffolderResourceLoader scaffolderResourceLoaderMock = Mockito.mock(ScaffolderResourceLoader.class);
-    final String exchangeJsonResourceURL = "resource::com.mycompany:raml-api:1.0.0:raml:zip:exchange.json";
-    final String rootRamlResourceURL = "resource::com.mycompany:raml-api:1.0.0:raml:zip:simple.raml";
+    final String ramlFolder = "src/test/resources/scaffolder/";
+    final String rootRaml = "simpleV10";
+
+    testSimple(ramlFolder, rootRaml);
+  }
 
 
-    Mockito.doReturn(getInputStream("src/test/resources/scaffolder/exchange.json")).when(scaffolderResourceLoaderMock)
-        .getResourceAsStream(exchangeJsonResourceURL);
-    Mockito.doReturn(getInputStream("src/test/resources/scaffolder/simple.raml")).when(scaffolderResourceLoaderMock)
-        .getResourceAsStream(rootRamlResourceURL);
+  @Test
+  public void testSimpleGenerationV10() throws IOException {
+    final String rootRaml = "simpleV10";
+    final String ramlFolder = "src/test/resources/scaffolder/";
 
+    testSimple(ramlFolder, rootRaml);
+  }
+
+  @Test
+  public void generateWithIncludes10() throws Exception {
+
+    final String rootRaml = "api";
+    final String ramlFolder = "src/test/resources/api-sync/scaffolder-include-10/";
+
+    File xmlOut = generateScaffolder(ramlFolder, rootRaml, Collections.singletonList("included.raml"));
+
+    assertTrue(xmlOut.exists());
+    String s = IOUtils.toString(new FileInputStream(xmlOut));
+    assertNotNull(s);
+    assertEquals(2, countOccurences(s, "http:response statusCode=\"#[vars.httpStatus default 200]\""));
+    assertEquals(2, countOccurences(s, "http:error-response statusCode=\"#[vars.httpStatus default 500]\""));
+    assertEquals(7, countOccurences(s, "<on-error-propagate"));
+    assertEquals(7, countOccurences(s, "<ee:message>"));
+    assertEquals(7, countOccurences(s, "<ee:variables>"));
+    assertEquals(4, countOccurences(s, "http:body"));
+    assertEquals(2, countOccurences(s, "#[payload]"));
+    assertEquals(7, countOccurences(s, "<ee:set-variable"));
+    assertEquals(7, countOccurences(s, "<ee:set-payload>"));
+    assertEquals(0, countOccurences(s, "interpretRequestErrors=\"true\""));
+    assertEquals(2, countOccurences(s, "post:\\Queue:application\\json:"+ ROOT_RAML_RESOURCE_URL + rootRaml + "-config"));
+    assertEquals(2, countOccurences(s, "post:\\Queue:text\\xml:"+ ROOT_RAML_RESOURCE_URL + rootRaml + "-config"));
+    assertEquals(2, countOccurences(s, "<logger level=\"INFO\" message="));
+  }
+
+  private void testSimple(String ramlFolder, String rootRaml) throws IOException {
     List<Dependency> dependencyList = new ArrayList<>();
-    dependencyList.add(createDependency("com.mycompany", "raml-api", "1.0.0", "raml", "zip"));
-    List<File> xmls = Arrays.asList();
-    File muleXmlOut = folder.newFolder("mule-xml-out");
-
-    new ScaffolderAPI().run(dependencyList, scaffolderResourceLoaderMock, muleXmlOut, null, DEFAULT_MULE_VERSION,
-                            DEFAULT_RUNTIME_EDITION);
-
-    File muleXmlSimple = new File(muleXmlOut, "resource::com.mycompany:raml-api:1.0.0:raml:zip:simple.xml");
+    dependencyList.add(dependency);
+    File muleXmlSimple = generateScaffolder(ramlFolder, rootRaml);
 
     assertTrue(muleXmlSimple.exists());
     String s = IOUtils.toString(new FileInputStream(muleXmlSimple));
-    assertEquals(1, countOccurences(s, "http:listener-config name=\"resource::com.mycompany:raml-api:1.0.0:raml:zip:simple"));
+    String listenerConfigName = ROOT_RAML_RESOURCE_URL + rootRaml;
+
+    assertSimple(s, listenerConfigName);
+  }
+
+  private File generateScaffolder(String ramlFolder, String rootRaml)
+      throws FileNotFoundException {
+    return generateScaffolder(ramlFolder, rootRaml, null);
+  }
+
+  private File generateScaffolder(String ramlFolder, String rootRaml, List<String> referencedFiles)
+      throws FileNotFoundException {
+    final String exchangeJsonResourceURL = ROOT_RAML_RESOURCE_URL + "exchange.json";
+    final String rootRamlResourceURL = ROOT_RAML_RESOURCE_URL + rootRaml + ".raml";
+
+    mockScaffolderResourceLoader(exchangeJsonResourceURL, ramlFolder, rootRaml + ".json");
+    mockScaffolderResourceLoader(rootRamlResourceURL, ramlFolder, rootRaml + ".raml");
+
+    if (referencedFiles != null) {
+      for (String file : referencedFiles) {
+        mockScaffolderResourceLoader(DEPENDENCIES_RESOURCE_URL + file, ramlFolder, file);
+      }
+    }
+
+    File muleXmlOut = folder.newFolder("mule-xml-out");
+
+    new ScaffolderAPI().run(Collections.singletonList(dependency), scaffolderResourceLoaderMock, muleXmlOut, null, MULE_4_VERSION,
+                            EE);
+
+    return new File(muleXmlOut, ROOT_RAML_RESOURCE_URL + rootRaml + ".xml");
+  }
+
+  private void mockScaffolderResourceLoader(String resourceURL, String folder, String file) throws FileNotFoundException {
+    Mockito.doReturn(getInputStream(folder + file)).doReturn(getInputStream(folder + file)).when(scaffolderResourceLoaderMock).getResourceAsStream(resourceURL);
+  }
+
+  private void assertSimple(String s, String listenerConfigName) {
+    assertEquals(1, countOccurences(s, "http:listener-config name=\"" + listenerConfigName));
     assertEquals(1, countOccurences(s, "http:listener-connection host=\"0.0.0.0\" port=\"8081\""));
     assertEquals(2, countOccurences(s, "http:listener "));
     assertEquals(0, countOccurences(s, "interpretRequestErrors=\"true\""));
@@ -136,8 +145,8 @@ public class ScaffolderApiSyncTest {
     assertEquals(4, countOccurences(s, "http:body"));
     assertEquals(2, countOccurences(s, "#[payload]"));
     assertEquals(8, countOccurences(s, "http:headers"));
-    //    assertEquals(2, countOccurences(s, "get:\\:" + rootRamlResourceURL + "-config"));
-    //    assertEquals(2, countOccurences(s, "get:\\pet:" + rootRamlResourceURL + "-config"));
+    assertEquals(2, countOccurences(s, "get:\\:" + listenerConfigName + "-config"));
+    assertEquals(2, countOccurences(s, "get:\\pet:" + listenerConfigName + "-config"));
     assertEquals(0, countOccurences(s, "extensionEnabled"));
     assertEquals(1, countOccurences(s, "apikit:console"));
     assertEquals(0, countOccurences(s, "consoleEnabled=\"false\""));
@@ -153,21 +162,11 @@ public class ScaffolderApiSyncTest {
     assertEquals(5, countOccurences(s, "<logger level=\"INFO\" message="));
   }
 
-  private InputStream getInputStream(String resourcePath) {
-    InputStream resourceStream = null;
-
-    try {
-      resourceStream = new FileInputStream(resourcePath);
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    }
-
-    return resourceStream;
+  private InputStream getInputStream(String resourcePath) throws FileNotFoundException {
+    return new FileInputStream(resourcePath);
   }
 
   private static Dependency createDependency(String groupId, String artifactId, String version, String classifier, String type) {
-
-
     Dependency dependency = new Dependency();
 
     dependency.setGroupId(groupId);
