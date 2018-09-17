c7ad6346f4cf38bce8d706441598f3d8692b8ba9
Juan Brasca <juan.brasca@gmail.com>
2018-09-17 11:48:38 -0300

    APIKIT-1568: Update Resource format for APISync resources

--- parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/loader/ApiSyncResourceLoader.java
+++ parser-wrapper/interface-impl-v2/src/main/java/org/mule/raml/implv2/loader/ApiSyncResourceLoader.java
@@ -17,7 +17,7 @@ import static java.lang.String.format;
 public class ApiSyncResourceLoader implements ResourceLoader {
 
   private ResourceLoader resourceLoader = new DefaultResourceLoader();
-  private static final String RESOURCE_FORMAT = "resource::%s:%s:%s:%s";
+  private static final String RESOURCE_FORMAT = "resource::%s:%s:%s:%s:%s:%s";
 
   @Nullable
   @Override
@@ -25,7 +25,8 @@ public class ApiSyncResourceLoader implements ResourceLoader {
     if (s.startsWith("/exchange_modules") || s.startsWith("exchange_modules")) {
       String[] resourceParts = s.split("/");
       int length = resourceParts.length;
-      return resourceLoader.fetchResource(format(RESOURCE_FORMAT, resourceParts[length - 4], resourceParts[length - 3],
+      return resourceLoader.fetchResource(format(RESOURCE_FORMAT, resourceParts[length - 6], resourceParts[length - 5],
+                                                 resourceParts[length - 4], resourceParts[length - 3],
                                                  resourceParts[length - 2], resourceParts[length - 1]));
     }
     return resourceLoader.fetchResource(s);
