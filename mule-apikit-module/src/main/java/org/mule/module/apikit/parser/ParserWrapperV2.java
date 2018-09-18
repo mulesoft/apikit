3194f7c0a030c3bd05b6125a943d4a2c78e11ae1
Juan Brasca <juan.brasca@gmail.com>
2018-09-18 14:41:23 -0300

    APIKIT-1511: Avoid ApiSyncResouloader initialization for console resources

--- mule-apikit-module/src/main/java/org/mule/module/apikit/parser/ParserWrapperV2.java
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/parser/ParserWrapperV2.java
@@ -111,4 +111,10 @@ public class ParserWrapperV2 implements ParserWrapper {
     // do nothing, as updates are not supported
     logger.debug("RAML 1.0 parser does not support base uri updates");
   }
+
+  @Override
+  public InputStream fetchResource(String resource) {
+
+    return resourceLoader.fetchResource(resource);
+  }
 }
