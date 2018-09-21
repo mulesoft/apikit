d7dd2c35b5b8d193a002d9bd068dcb98937884d9
Juan Brasca <juan.brasca@gmail.com>
2018-09-21 15:15:36 -0300

    APIKIT-1489: Improve ApiKit Scaffolding returning information to be added in Amplitude (#371)

---
+++ mule-apikit-tools/mule-apikit-scaffolder/src/main/java/org/mule/tools/apikit/model/ScaffolderReport.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+package org.mule.tools.apikit.model;
+
+public class ScaffolderReport {
+
+  private String vendorId;
+  private String version;
+
+  private String status;
+
+  public static String SUCCESS= "success";
+  public static String FAILED= "failed";
+
+  public String getVendorId() {
+    return vendorId;
+  }
+
+  public void setVendorId(String vendorId) {
+    this.vendorId = vendorId;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
