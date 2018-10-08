b7eda9c1cea8a35c33da7c36ffc43da00fc09f2e
dfmacias <elmellidiego@gmail.com>
2018-10-08 16:55:57 -0300

    APIKIT-1451: Implement tests to check scalars type validation

---
+++ mule-apikit-module/src/test/java/org/mule/module/apikit/types/ScalarTypesValidationTestCase.java
@@ -0,0 +1,106 @@
+package org.mule.module.apikit.types;
+
+import org.junit.Test;
+import org.mule.module.apikit.AbstractMultiParserFunctionalTestCase;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.core.Is.is;
+
+public class ScalarTypesValidationTestCase extends AbstractMultiParserFunctionalTestCase {
+
+    @Override
+    protected String getConfigFile() {
+        return "org/mule/module/apikit/types/mule-config.xml";
+    }
+
+    @Test
+    public void testAlphanumericForStringParam() {
+        shouldReturnOkFor("str", "abc123");
+    }
+
+    @Test
+    public void testSendStrNumberForStringParam() {
+        shouldReturnOkFor("str", "1234");
+    }
+
+    @Test
+    public void testSendNumberForStringParam() {
+        shouldReturnOkFor("str", 1234);
+    }
+
+    @Test
+    public void testSendBooleanForStringParam() {
+        shouldReturnOkFor("str", true);
+    }
+
+    @Test
+    public void testSendDateForStringParam() {
+        shouldReturnOkFor("str", "2016-02-28T16:41:41.090Z");
+    }
+
+    @Test
+    public void testSendNumberForNumberParam() {
+        shouldReturnOkFor("num", 1234.9);
+    }
+
+    @Test
+    public void testSendStrNumberForNumberParam() {
+        shouldReturnOkFor("num", "1234.9");
+    }
+
+    @Test
+    public void testSendIntForIntegerParam() {
+        shouldReturnOkFor("int", 1234);
+    }
+
+    @Test
+    public void testSendStrNumberForIntegerParam() {
+        shouldReturnOkFor("int", "1234");
+    }
+
+    @Test
+    public void testSendBoolForBoolParam() {
+        shouldReturnOkFor("bool", true);
+    }
+
+    @Test
+    public void testSendStrBoolForBoolrParam() {
+        shouldReturnOkFor("bool", "false");
+    }
+
+    @Test
+    public void testSendDateOnlyForDateOnlyParam() {
+        shouldReturnOkFor("dateOnly", "2015-05-23");
+    }
+
+    @Test
+    public void testSendTimeForTimeParam() {
+        shouldReturnOkFor("time", "12:30:00");
+    }
+
+    @Test
+    public void testSendDatetimeOnlyForDatetimeOnlyParam() {
+        shouldReturnOkFor("datetimeOnly", "2015-07-04T21:00:00");
+    }
+
+    @Test
+    public void testSendDatetimeForDatetimeParam() {
+        shouldReturnOkFor("datetime", "2016-02-28T16:41:41.090Z");
+    }
+
+    private void shouldReturnOkFor(String param, Object value) {
+        final String response = "{\n" +
+                "  \"" + param + "\": \"" + String.valueOf(value) + "\"\n" +
+                "}";
+
+        shouldReturnFor(param, value, 200, response);
+    }
+
+    private void shouldReturnFor(String param, Object value, int statusCode, String response) {
+        given()
+                .queryParam(param, value)
+                .expect().response().statusCode(statusCode)
+                .body(is(response))
+                .when().get("/api/resources");
+    }
+}
