ce75a60960f6f35e39e9406d3b4a15b624e04a2f
dfmacias <elmellidiego@gmail.com>
2018-11-06 15:55:03 -0300

    Preparing next iteration (1.1.12-SNAPSHOT)

--- mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
+++ mule-apikit-module/src/main/java/org/mule/module/apikit/ApikitExtensionLoadingDelegate.java
@@ -31,7 +31,7 @@ public class ApikitExtensionLoadingDelegate implements ExtensionLoadingDelegate 
   public static final String PREFIX_NAME = "apikit";
   public static final String EXTENSION_DESCRIPTION = "APIKit plugin";
   public static final String VENDOR = "Mulesoft";
-  public static final String VERSION = "1.1.10-SNAPSHOT";
+  public static final String VERSION = "1.1.12-SNAPSHOT";
   public static final String XSD_FILE_NAME = "mule-apikit.xsd";
   private static final String UNESCAPED_LOCATION_PREFIX = "http://";
   private static final String SCHEMA_LOCATION = "www.mulesoft.org/schema/mule/mule-apikit";
