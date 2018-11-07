a2383fe819abf7f0a67ea2f55bba7af58d122228
Alejandro Amura <aamura@gmail.com>
2018-11-07 18:00:23 -0300

    creating Test to reproduce this problem

--- parser-wrapper/parser-service/src/test/java/org/mule/parser/service/ParserServiceTestCase.java
+++ parser-wrapper/parser-service/src/test/java/org/mule/parser/service/ParserServiceTestCase.java
@@ -8,14 +8,25 @@ package org.mule.parser.service;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Map;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mule.raml.interfaces.ParserType;
 import org.mule.raml.interfaces.ParserWrapper;
 import org.mule.raml.interfaces.model.ApiRef;
+import org.mule.raml.interfaces.model.IAction;
+import org.mule.raml.interfaces.model.IActionType;
+import org.mule.raml.interfaces.model.IMimeType;
+import org.mule.raml.interfaces.model.IRaml;
+import org.mule.raml.interfaces.model.IResource;
+import org.mule.raml.interfaces.model.IResponse;
 
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.mule.raml.interfaces.model.ApiVendor.OAS_20;
@@ -57,7 +68,7 @@ public class ParserServiceTestCase {
     assertThat(wrapper.getApiVendor(), is(OAS_20));
   }
 
-  @Ignore
+  @Test
   public void oasYaml20Wrapper() throws URISyntaxException {
 
     final URI api = resource("/petstore.yaml");
@@ -68,6 +79,33 @@ public class ParserServiceTestCase {
     assertThat(wrapper.getApiVendor(), is(OAS_20));
   }
 
+    @Ignore
+    public void oasJson20Examples() throws URISyntaxException {
+
+        final URI api = resource("/api-with-examples.json");
+
+        final ParserWrapper wrapper = ParserService.create(ApiRef.create(api), ParserType.AMF);
+        assertNotNull(wrapper);
+        assertThat(wrapper.getParserType(), is(ParserType.AMF));
+        assertThat(wrapper.getApiVendor(), is(OAS_20));
+
+        final IRaml raml = wrapper.build();
+        final Map<String, IResource> resources = raml.getResources();
+        
+        assertThat(resources.size(), is(2));
+
+        final Map<IActionType, IAction> actions = resources.get("/").getActions();
+        assertThat(actions.size(), is(1));
+
+        final IAction action = actions.get(IActionType.GET);
+        final Map<String, IResponse> responses = action.getResponses();
+        final String example = getExampleWrapper(responses);
+
+        System.out.println("ParserServiceTestCase.oasJson20Example\n" + example);
+        assertThat(example, not(isEmptyOrNullString()));
+
+    }
+
   private static URI resource(final String path) {
     URI result = null;
     try {
@@ -77,4 +115,45 @@ public class ParserServiceTestCase {
     }
     return result;
   }
+
+  // Same code used for Scaffolding
+    private String getExampleWrapper(Map<String, IResponse> responses) {
+
+        IResponse response = responses.get("200");
+
+        if (response == null || response.getBody() == null) {
+            for (IResponse response1 : responses.values()) {
+                if (response1.getBody() != null) {
+                    Map<String, IMimeType> responseBody1 = response1.getBody();
+                    IMimeType mimeType = responseBody1.get("application/json");
+                    if (mimeType != null && mimeType.getExample() != null) {
+                        return mimeType.getExample();
+                    } else {
+                        for (IMimeType type : responseBody1.values()) {
+                            if (type.getExample() != null) {
+                                return type.getExample();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (response != null && response.getBody() != null) {
+            Map<String, IMimeType> body = response.getBody();
+            IMimeType mimeType = body.get("application/json");
+            if (mimeType != null && mimeType.getExample() != null) {
+                return mimeType.getExample();
+            }
+
+            for (IMimeType mimeType2 : response.getBody().values()) {
+                if (mimeType2 != null && mimeType2.getExample() != null) {
+                    return mimeType2.getExample();
+                }
+            }
+        }
+
+        return null;
+
+    }
 }
