7e59cd98e6235be0cbec8f99f6261b4c9157eace
Diego Macias <diego.macias@diegomacias-ltm.internal.salesforce.com>
2018-12-21 14:59:07 -0300

    APIKIT-1707: Moving checking of disableValidations' flag to RequestValidator class

---
+++ mule-apikit-module/src/test/java/org/mule/module/apikit/RouterWithDisabledValidationsTestCase.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
+ * The software in this package is published under the terms of the CPAL v1.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+package org.mule.module.apikit;
+
+import com.jayway.restassured.RestAssured;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+import org.mule.tck.junit4.rule.DynamicPort;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+public class RouterWithDisabledValidationsTestCase extends MuleArtifactFunctionalTestCase {
+
+  @Override
+  protected String getConfigFile() {
+    return "org/mule/module/apikit/validation/disabled-validations/simple.xml";
+  }
+
+  @Rule
+  public DynamicPort serverPort = new DynamicPort("serverPort");
+
+  @Override
+  public int getTestTimeoutSecs() {
+    return 6000;
+  }
+
+  @Override
+  protected void doSetUp() throws Exception {
+    RestAssured.port = serverPort.getNumber();
+    super.doSetUp();
+  }
+
+  @Test
+  public void simpleRouting() throws Exception {
+    given().header("Accept", "*/*")
+            .body("something that won't be validated")
+        .expect()
+        .response().body(is("something that won't be validated"))
+        .statusCode(201)
+        .when().post("/api/resource/invalid");
+  }
+}
