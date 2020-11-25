c7a94b02d64dea6d57ef2f6fd04342ca99baaae5
Damián Courtil <dcourtil@mulesoft.com>
2020-11-25 18:20:27 -0300

    APIKIT-2817: Bump up some dependency versions (#579)
    * Bump junit from 4.12 to 4.13.1 in /mule-module-apikit (#569)
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump junit from 4.12 to 4.13.1 in /apikit-examples/leagues (#568)
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump junit from 4.8.2 to 4.13.1 in /apikit-tools/apikit-maven-plugin (#567)
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.8.2 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.13.1.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.8.2...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump junit from 4.8.2 to 4.13.1 in /apikit-tools/apikit-scaffolder (#566)
    
    * Bump junit from 4.8.2 to 4.13.1 in /apikit-tools/apikit-scaffolder
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.8.2 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.13.1.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.8.2...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Fix tests
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Damian Courtil <dcourtil@mulesoft.com>
    
    * Bump junit from 4.12 to 4.13.1 (#565)
    
    Bumps [junit](https://github.com/junit-team/junit4) from 4.12 to 4.13.1.
    - [Release notes](https://github.com/junit-team/junit4/releases)
    - [Changelog](https://github.com/junit-team/junit4/blob/main/doc/ReleaseNotes4.12.md)
    - [Commits](https://github.com/junit-team/junit4/compare/r4.12...r4.13.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * APIKIT-2817: Bump up parser versions (#574)
    
    * APIKIT-2817: Bump up Jackson version (#575)
    
    * APIKIT-2817: Bump up rest-assured (#576)
    
    * APIKIT-2817: Bump up plexus-utils (#577)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- mule-module-apikit/src/main/java/org/mule/module/apikit/validation/io/JsonUtils.java
+++ mule-module-apikit/src/main/java/org/mule/module/apikit/validation/io/JsonUtils.java
@@ -8,7 +8,9 @@ package org.mule.module.apikit.validation.io;
 
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 
 import java.io.IOException;
 import java.io.Reader;
@@ -16,18 +18,20 @@ import java.io.Reader;
 public class JsonUtils
 {
     private static final String JSON_STRICT_DUPLICATE_DETECTION_PROPERTY = "yagi.json_duplicate_keys_detection";
+    private static final String JSON_BLOCK_UNSAFE_POLYMORPHIC_BASE_TYPES_PROPERTY = "yagi.json_block_unsafe_polymorphic_base_types";
 
-    public static JsonNode parseJson(Reader reader) throws IOException
-    {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.disableDefaultTyping();
-        mapper.configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, getSystemPropValue());
+    public static JsonNode parseJson(Reader reader) throws IOException {
+        ObjectMapper mapper = JsonMapper.builder()
+                .deactivateDefaultTyping()
+                .configure(JsonParser.Feature.STRICT_DUPLICATE_DETECTION, getSystemPropValue(JSON_STRICT_DUPLICATE_DETECTION_PROPERTY))
+                .configure(MapperFeature.BLOCK_UNSAFE_POLYMORPHIC_BASE_TYPES,
+                        getSystemPropValue(JSON_BLOCK_UNSAFE_POLYMORPHIC_BASE_TYPES_PROPERTY))
+                .build();
         return mapper.readValue(reader, JsonNode.class);
     }
 
-    private static boolean getSystemPropValue()
-    {
-        return Boolean.valueOf(System.getProperty(JSON_STRICT_DUPLICATE_DETECTION_PROPERTY, "true"));
+    private static boolean getSystemPropValue(String property) {
+        return Boolean.valueOf(System.getProperty(property, "true"));
     }
 
 }
